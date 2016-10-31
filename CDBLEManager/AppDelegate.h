8575d0631dba8bebe9ae0ea7398804ca68c89370
chendi <bbigcd@163.com>
2016-10-31 18:45:39 +0800

    Initial Commit

---
+++ CDBLEManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CDBLEManager
+//
+//  Created by bbigcd on 16/10/31.
+//  Copyright © 2016年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CDBLEManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CDBLEManager
+//
+//  Created by bbigcd on 16/10/31.
+//  Copyright © 2016年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
