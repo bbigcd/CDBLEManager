29f9b463786e65af254e3bc3bd748d4235282fa4
chendi <bbigcd@163.com>
2016-11-03 16:19:42 +0800

    change file line
    change some file line to finder

---
+++ CDBLEManager/Classes/Controller/SeviceViewController.h
@@ -0,0 +1,13 @@
+//
+//  SeviceViewController.h
+//  CDBLEManager
+//
+//  Created by bbigcd on 16/11/3.
+//  Copyright © 2016年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SeviceViewController : UIViewController
+
+@end
